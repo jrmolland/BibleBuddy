dfe243dc4b01a6dd144022125e38ae5f2323ad02
Jacob Molland <jacobmolland28@gmail.com>
2021-04-23 20:15:30 -0500

    Started firebase login auth

---
+++ app/src/main/java/com/biblebuddy/ui/login/LoginViewModelFactory.kt
@@ -0,0 +1,25 @@
+package com.biblebuddy.ui.login
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.biblebuddy.data.LoginDataSource
+import com.biblebuddy.data.LoginRepository
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+class LoginViewModelFactory : ViewModelProvider.Factory {
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LoginViewModel::class.java)) {
+            return LoginViewModel(
+                loginRepository = LoginRepository(
+                    dataSource = LoginDataSource()
+                )
+            ) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
